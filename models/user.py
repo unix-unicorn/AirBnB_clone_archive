381afb527eea2a58cb1f7da831b6da4da3eaf85e
amna <amnataha2011@gmil.com>
2023-08-12 02:44:06 -0700

    mmff

--- models/user.py
+++ models/user.py
@@ -5,9 +5,9 @@ class User(BaseModel):
     my new class user
     """
     email = ""
-    self.password = ""
-    self.first_name = ""
-    self.last_name = ""
+    password = ""
+    first_name = ""
+    last_name = ""
     def __init__(self, *args, **kwargs):
         """my initionalize function """
         super().__init__(*args, **kwargs)
